7c916ab3ee5708096064412477417777cc44784f
Evan Dorn <evan@PBG4.gateway.2wire.net>
2008-09-04 09:27:47 -0700

    solution to 9

--- 009/E009.rb
+++ 009/E009.rb
@@ -1,8 +1,22 @@
 require 'euler_solution.rb'
 class E009 < EulerSolution
   
+  LIMIT = 1000
+  
   def is_pythagorean_triple?(a,b,c)
     (a * a) + (b * b) == (c * c)
   end
-  
+
+  def answer
+    for c in (1..(LIMIT-2))
+      for a in (1..(LIMIT-1-c))
+        b = LIMIT - a - c
+        if is_pythagorean_triple?(a, b, c) 
+          #puts "#{a} #{b} #{c} "+ (a + b + c).to_s
+          return a * b * c 
+        end
+      end
+    end
+    0
+  end
 end
