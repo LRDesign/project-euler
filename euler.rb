76a5c73895ffad9bf69c05a838425efcd720c3be
Evan Dorn <evan@PBG4.gateway.2wire.net>
2008-09-04 09:03:55 -0700

    more cleanup of framework

--- euler.rb
+++ euler.rb
@@ -4,12 +4,19 @@ $: << File.join(File.expand_path(File.dirname(__FILE__), "lib"))
 require 'tools'
 include Tools
 
+def usage_message
+  puts "Usage: euler.rb [number] [all]"
+  exit
+end
+
+usage_message unless ARGV.size > 0
+
 while (arg = ARGV.shift)
   if is_a_number?(arg)
     solve_one(arg.to_i)
   elsif arg == 'all'
     puts "Solve all not implemented yet"
   else
-    puts "Usage: euler.rb [number] [all]"
+    usage_message
   end
 end
