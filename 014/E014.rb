b921009e835c4176818ab215e754a2002b3ded8d
Evan Dorn <evan@PBG4.gateway.2wire.net>
2008-09-05 19:59:31 -0700

    initial structure for #14

---
+++ 014/E014.rb
@@ -0,0 +1,5 @@
+require 'euler_solution.rb'
+class E014 < EulerSolution
+  LIMIT = 1000000
+  
+end
---
+++ 014/E014_spec.rb
@@ -0,0 +1,12 @@
+require File.dirname(__FILE__) + "/../spec/spec_helper.rb"
+require File.dirname(__FILE__) + "/E014"
+
+describe "E014" do
+  before(:each) do
+    @es = E014.new
+  end
+  
+end
+
+  
+  
