135145a1ada204a370aef2abcd9e7fbf42ee88ae
Evan Dorn <evan@PBG4.gateway.2wire.net>
2008-09-05 23:07:01 -0700

    correct solution to 14

--- 014/E014.rb
+++ 014/E014.rb
@@ -1,5 +1,57 @@
 require 'euler_solution.rb'
+
+class Fixnum 
+  N_BYTES = [42].pack('i').size 
+  N_BITS = N_BYTES * 8 
+  MAX = 2 ** (N_BITS - 2) - 1 
+  MIN = -MAX - 1 
+end 
+
 class E014 < EulerSolution
   LIMIT = 1000000
+      
+  def step(num)
+    if num % 2 == 0
+      num / 2
+    else
+      (num * 3) + 1
+    end
+  end
+  
+  def chain_length(num)
+    count = 1
+    until num == 1
+      #print "\t#num: #{num} count: #{count}"
+      if (num > Fixnum::MAX) or @steps[num].nil?
+        num = step(num)
+        count += 1  
+        #puts " added 1"    
+      else
+        count += @steps[num] - 1
+        #puts " cached added #{@steps[num]}"        
+        break
+      end  
+    end
+    count
+  end
+  
+  def answer
+    @steps = Array.new
+    longest = 0 
+    max = 0
+    for i in (1..LIMIT)
+      #puts "Testing #{i}:"
+      length  = chain_length(i)
+      #puts "result #{i}: #{length}"
+      @steps[i] = length
+      #puts "wrote #{length} to steps[#{i}]"
+      if length > max
+        longest = i
+        max = length
+      end 
+    end
+    puts "longest: #{longest} max: #{max}"
+    longest
+  end
   
 end
--- 014/E014_spec.rb
+++ 014/E014_spec.rb
@@ -6,6 +6,30 @@ describe "E014" do
     @es = E014.new
   end
   
+  describe "step" do
+    it "should return 5 when given 10" do
+     @es.step(10).should == 5 
+    end
+    
+    it "should return 166 when given 55" do
+      @es.step(55).should == 166
+    end
+  end
+  
+  describe "chain_length" do
+    it "should return 7 when given 10" do
+      @es.chain_length(10).should == 7
+    end
+    
+    it "should return 10 when given 13" do
+      @es.chain_length(13).should == 10      
+    end
+    
+    it "should return 114 when given 110" do
+      @es.chain_length(110).should == 114
+    end
+  end
+  
 end
 
   
