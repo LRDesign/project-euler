529d8bff0f7c132be55f71ef09b6b76ba40f4e07
Evan Dorn <evan@evan-dorns-power-mac-g5.local>
2008-09-07 21:48:34 -0700

    solution to 18

---
+++ 018/E018.rb
@@ -0,0 +1,40 @@
+require 'euler_solution.rb'
+class E018 < EulerSolution
+
+TRIANGLE = "75
+95 64
+17 47 82
+18 35 87 10
+20 04 82 47 65
+19 01 23 75 03 34
+88 02 77 73 07 63 67
+99 65 04 28 06 16 70 92
+41 41 26 56 83 40 80 70 33
+41 48 72 33 47 32 37 16 94 29
+53 71 44 65 25 43 91 52 97 51 14
+70 11 33 28 77 73 17 78 39 68 17 57
+91 71 52 38 17 14 91 43 58 50 27 29 48
+63 66 04 68 89 53 67 30 73 16 69 87 40 31
+04 62 98 27 23 09 70 98 73 93 38 53 60 04 23"
+
+  def zn(n)
+    n.nil? ? 0 : n.to_i
+  end
+
+  def answer  
+    foo = TRIANGLE.split(/$/)
+    tri = foo.inject(Array.new) { |arr, row| arr << row.split }           
+    #tri.each {|r| puts r.join(', ') }   
+    dist = Array.new
+    (0...tri.size).each do |rn|
+      dist[rn]=Array.new
+      (0...tri[rn].size).each do |en|
+        cur = zn(tri[rn][en])
+        dist[rn][en] =  cur + [zn(dist[rn-1][en]), zn(dist[rn-1][en-1])].max 
+      end
+    end
+    #dist.each {|r| puts r.join(', ') }   
+    dist[dist.size-1].max
+  end
+
+end
